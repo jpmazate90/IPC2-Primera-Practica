3488a1b1a92281accca9132e3da9882f69f25f86
jpmazate90 <41486500+jpmazate90@users.noreply.github.com>
2018-07-24 23:11:40 -0600

    Update IPC2Calculadora.java
    primer commit 3.0

--- src/ipc2/calculadora/IPC2Calculadora.java
+++ src/ipc2/calculadora/IPC2Calculadora.java
@@ -6,6 +6,7 @@ public class IPC2Calculadora {
     public static void main(String[] args) {
         System.out.println("Hola je");
         System.out.println("hola");
+        System.out.println("como estas");
     }
     
 }
