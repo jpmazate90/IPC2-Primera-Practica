8f4a4f109fbaa1214906db80257d077d1e4277f0
jpmazate90 <juanpablo-orizabalgil@cunoc.edu.gt>
2018-07-25 11:27:34 -0600

    creacion de la interfaz grafica

--- src/ipc2/calculadora/IPC2Calculadora.java
+++ src/ipc2/calculadora/IPC2Calculadora.java
@@ -1,12 +1,11 @@
 
 package ipc2.calculadora;
 
+
 public class IPC2Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hola je");
-        System.out.println("hola");
-        System.out.println("como estas");
+        InterfazGrafica interfaz = new InterfazGrafica();
     }
     
 }
